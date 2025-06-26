016e9c875c132355741adb90093849076db8f3cb
EduardCRadd <105921610+EduardCRadd@users.noreply.github.com>
2025-06-26 12:36:14 +0100

    Update hunt experience

--- src/app/hunt/layout.tsx
+++ src/app/hunt/layout.tsx
@@ -2,38 +2,38 @@
 import { usePathname, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
-// import HeaderBg from '@/components/header/HeaderBg'
-// import { HuntTermsProvider } from '@/components/scavengerHunt/HuntTermsProvider'
-// import useLayoutStore from '@/hooks/useLayoutStore'
-// import useScavengerHuntStore from '@/hooks/useScavengerHuntStore'
+import HeaderBg from '@/components/header/HeaderBg'
+import { HuntTermsProvider } from '@/components/scavengerHunt/HuntTermsProvider'
+import useLayoutStore from '@/hooks/useLayoutStore'
+import useScavengerHuntStore from '@/hooks/useScavengerHuntStore'
 import { Pathname } from '@/resources/pathname'
 
 function ScavengerHuntLayout({ children }: { children: React.ReactNode }) {
   const { push } = useRouter()
 
-  useEffect(() => {
-    push(Pathname.Paint)
-  }, [push])
-
-  return null
-
-  // const showMenuButton = useLayoutStore((s) => s.showMenuButton)
-  // const pathname = usePathname()
-  // const isHuntPage = pathname === Pathname.ScavengerHunt
-
-  // const setIncludeScavengerLinkInMenu = useScavengerHuntStore((s) => s.setIncludeScavengerLinkInMenu)
-
   // useEffect(() => {
-  //   setIncludeScavengerLinkInMenu(true)
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [])
+  //   push(Pathname.Paint)
+  // }, [push])
 
-  // return (
-  //   <HuntTermsProvider>
-  //     <HeaderBg show={showMenuButton && isHuntPage} />
-  //     {children}
-  //   </HuntTermsProvider>
-  // )
+  // return null
+
+  const showMenuButton = useLayoutStore((s) => s.showMenuButton)
+  const pathname = usePathname()
+  const isHuntPage = pathname === Pathname.ScavengerHunt
+
+  const setIncludeScavengerLinkInMenu = useScavengerHuntStore((s) => s.setIncludeScavengerLinkInMenu)
+
+  useEffect(() => {
+    setIncludeScavengerLinkInMenu(true)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <HuntTermsProvider>
+      <HeaderBg show={showMenuButton && isHuntPage} />
+      {children}
+    </HuntTermsProvider>
+  )
 }
 
 export default ScavengerHuntLayout
